040ec5c098f1c2eb80f36e50f6e2093c7710fdee
leoljunior <juhhinx@gmail.com>
2022-05-27 10:28:25 -0300

    created cache feature

--- src/main/java/com/leonardo/forumapi/ForumApiApplication.java
+++ src/main/java/com/leonardo/forumapi/ForumApiApplication.java
@@ -2,10 +2,12 @@ package com.leonardo.forumapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
+@EnableSpringDataWebSupport //permitir o uso da paginação
+@EnableCaching //permitir o cache
 public class ForumApiApplication {
 
 	public static void main(String[] args) {
